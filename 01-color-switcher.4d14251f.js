04cb1e79a9cbe7d29557c120f337d4ac7dba20a1
Bakuzov <100297704+Bakuzov@users.noreply.github.com>
2022-06-29 18:12:34 +0000

    Deploying to gh-pages from @ Bakuzov/goit-js-hw-09@a15b336352fc4aa7cc21ff40a35c52f47104fe0f 🚀

---
+++ 01-color-switcher.4d14251f.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("body"),e=document.querySelector("button[data-start]"),n=document.querySelector("button[data-stop]");function d(){t.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16))}e.addEventListener("click",(function(){timerId=setInterval(d,1e3),e.disabled=!0,n.disabled=!1})),n.addEventListener("click",(function(){clearInterval(timerId),e.disabled=!1,n.disabled=!0}))}();
+//# sourceMappingURL=01-color-switcher.4d14251f.js.map
